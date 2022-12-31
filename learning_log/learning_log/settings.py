37c85acd1f58476f653125535439f15ab5dfbb94
blake-dreyer <blakedreyerdev@gmail.com>
2022-12-31 11:49:35 -0500

    Initial DB structure set up with two Django Models

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My apps
+    'learning_logs',
+    #Default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ learning_log/learning_logs/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Topic(models.Model):
+    """A Topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
+    
+class Entry(models.Model):
+    """Something specific the user learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Return a string representation of a model."""
+        return f"{self.text[:50]}..."
+
+    
+
