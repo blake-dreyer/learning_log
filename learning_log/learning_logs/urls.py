55088f2571faf7cdd97545cc1aa2ca4a2cd26994
blake-dreyer <blakedreyerdev@gmail.com>
2023-01-02 14:14:55 -0500

    Setup Initial URL Mapping and index Made multiple changes to setup the projects URL mapping. Built an index .html page with a very simple template. Project succesfully loads index on local server

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Define the URL Pattern for Learning Logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    #Home Page
+    path('', views.index, name='index'),
+]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """The homepage for Learning Log."""
+    return render(request, 'learning_logs/index.html')
