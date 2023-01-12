47d0083ed79095913f3bb2e3fd652820b0a02ed4
blake-dreyer <blakedreyerdev@gmail.com>
2023-01-12 15:28:31 -0500

    Added user login functiionality

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,6 +1,6 @@
 """Define the URL Pattern for Learning Logs"""
 
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
---
+++ learning_log/users/urls.py
@@ -0,0 +1,9 @@
+"""Defines URL Pattern for users"""
+
+from django.urls import path, include
+
+app_name = 'users'
+urlpatterns = [
+    #Include default auth urls.
+    path('', include ('django.contrib.auth.urls')),
+]
