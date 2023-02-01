6c4d12060ddba4658e9841bfa2814f4d99f6c967
blake-dreyer <blakedreyerdev@gmail.com>
2023-01-31 21:24:27 -0500

    refined eb deployment

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -81,24 +81,33 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    'default': {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+    
+        'default': {
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'pgll',
+            'NAME': 'pgll',
 
-        'USER': 'blake-dreyer',
+            'USER': 'blake-dreyer',
 
-        'PASSWORD': '',
+            'PASSWORD': '',
 
-        'HOST': 'localhost',
+            'HOST': 'localhost',
 
-        'PORT': '5432',
+            'PORT': '5432',
 
     }
 
@@ -141,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Static Root variable for elastic beanstalk deployment
+
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
